5e8f69450a81a4a4e808b97a4d0b848e9840f7f8
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-11-16 14:18:44 +0330

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <title>SnappFood</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <div>Initial Edit</div>
+    </>
+  );
+};
+
+export default index;
