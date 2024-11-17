d67f6211e88b537c4e65ee377395b9deb0ad5fd5
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-11-17 21:51:36 +0330

    clean code (MenuPage > Card)

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,31 @@ import Location from "@/icons/Location";
 import Link from "next/link";
 // import styles
 import styles from "@/styles/Card.module.css";
-
+const { card, foodPrice, foodDetails, foodDiscount, badge } = styles;
 const Card = ({ item }) => {
   const { id, name, price, details, discount } = item;
 
   return (
-    <div className={styles.container}>
-      <img src={`/images/${id}.jpeg`} alt={name} />
-      <div className={styles.details}>
-        <h4>{name}</h4>
+    <div className={card}>
+      <img src={`/images/${id}.jpeg`} alt={name + " image"} />
+      <div className={foodDetails}>
+        <h3>{name}</h3>
         <div>
           <Location />
           {details[0].Cuisine}
         </div>
       </div>
-      <div className={styles.price}>
+      <div className={foodPrice}>
         <Dollar />
-        {discount ? <span className={styles.discount}>{(price * (100 - discount)) / 100}$</span> : <span>{price}$</span>}
-        {discount ? <div className={styles.badge}>{discount}%</div> : null}
+        {discount ? (
+          <span className={foodDiscount}>
+            <span style={{ textDecoration: "line-through", color: "#000" }}>{price + ` `}</span>
+            {(price * (100 - discount)) / 100}$
+          </span>
+        ) : (
+          <span>{price}$</span>
+        )}
+        {discount ? <div className={badge}>{discount}%</div> : null}
       </div>
       <Link href={`/menu/${id}`} className="btn btn-primary">
         See Details
