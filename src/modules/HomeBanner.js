f48228e226500f77ebdea1acb2963add3ade7b6f
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-11-20 22:24:44 +0330

    responsive home page

--- src/modules/HomeBanner.js
+++ src/modules/HomeBanner.js
@@ -8,17 +8,19 @@ import Link from "next/link";
 
 const HomeBanner = () => {
   return (
-    <div className={banner}>
-      <div>
-        <h1>SnappFood</h1>
-        <h2>Food Delivery and Takeout!</h2>
-        <p className="lead">Snappfood is an online food ordering and delivery platform launched by Snapp in 2014. Meals are delivered by couriers using cars, scooters, bikes, or on foot.</p>
-        <button className="btn btn-primary">
-          <Link href={`/menu`}>See All</Link>
-        </button>
+    <>
+      <h1 className="title-line">SnappFood</h1>
+      <div className={banner}>
+        <div>
+          <h2>Food Delivery and Takeout!</h2>
+          <p className="lead">Snappfood is an online food ordering and delivery platform launched by Snapp in 2014. Meals are delivered by couriers using cars, scooters, bikes, or on foot.</p>
+          <button className="btn btn-primary">
+            <Link href={`/menu`}>See All</Link>
+          </button>
+        </div>
+        <Image src={bannerImg} width={500} height={370} alt="Banner Image" />
       </div>
-      <Image src={bannerImg} width={500} height={370} alt="Banner Image" />
-    </div>
+    </>
   );
 };
 
